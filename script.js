a3ebc9d095f8edcdd87b26217392f81193b4baaf
iChristmas8 <ichristmas@yandex.ru>
2022-01-28 11:10:11 +0500

    добавил значения переменным

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'project-JS';
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 20;
+let rollback = 8;
+let fullPrice = 100000;
+let adaptive = 10 > 8;
 
-alert("Hello World");
+alert(screens);
 console.log("Hello World");
