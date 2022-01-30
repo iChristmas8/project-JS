94b2fe7e3ce666f34635cee1fc43b7dbc37fa55d
iChristmas8 <ichristmas@yandex.ru>
2022-01-30 13:01:50 +0500

    Присвоил значения к переменным и вывел в консоль

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let title = 'project-JS';
-let screens = "Простые, Сложные, Интерактивные";
+let title = "project-JS";
+let screens = ["Простые, Сложные, Интерактивные"];
 let screenPrice = 20;
-let rollback = 8;
-let fullPrice = 100000;
-let adaptive = 10 > 8;
+let rollback = 80;
+let fullPrice = 100;
+let adaptive = true;
 
-alert(screens);
-console.log("Hello World");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(`${screenPrice} рублей, ${fullPrice} рублей`);
+console.log(screens[0].toLowerCase());
+console.log(screens);
+console.log(fullPrice * (rollback/100));
