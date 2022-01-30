24682fc884d1c6f5947adda490d00b8cf7fb4210
iChristmas8 <ichristmas@yandex.ru>
2022-01-30 13:17:13 +0500

    Поправил строку 13, нашел верное решение

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let title = "project-JS";
-let screens = ["Простые, Сложные, Интерактивные"];
+let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 20;
 let rollback = 80;
 let fullPrice = 100;
@@ -9,7 +9,6 @@ console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
 console.log(screens.length);
-console.log(`${screenPrice} рублей, ${fullPrice} рублей`);
-console.log(screens[0].toLowerCase());
-console.log(screens);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей, Стоимость разработки сайта ${fullPrice} рублей`);
+console.log(screens.toLowerCase().split(", "));
 console.log(fullPrice * (rollback/100));
