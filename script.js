8b6783bf7c103978a038edc17cd68dc548576988
iChristmas8 <ichristmas@yandex.ru>
2022-02-02 18:53:04 +0500

    выполнил урок 4

--- script.js
+++ script.js
@@ -1,16 +1,62 @@
 'use strict';
-let title = "project-JS";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 20;
-let rollback = 80;
-let fullPrice = 100;
-let adaptive = true;
+let title = prompt("Как называется Ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать? (пример: 'Простые, Сложные, Интерактивные')");
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
+let rollback = 15;
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log(`Стоимость верстки экранов ${screenPrice} рублей, Стоимость разработки сайта ${fullPrice} рублей`);
+
+
+let showTypeOf = function(variable) {
+    console.log(variable, typeof variable);
+};
+
+let getRollbackMessage = function(price) {
+    if(price >= 30000) {
+        return "Даем скидку в 10%";
+    } else if (price >= 15000 && price < 30000) {
+        return "Даем скидку в 5%";
+    } else if (price < 15000 && price >= 0) {
+        return  "Скидка не предусмотрена";
+    } else if (price < 0) {
+        return "Что то пошло не так";
+    }
+};
+
+let getAllServicePrices = function() {
+    return servicePrice1 + servicePrice2;         //Function Expression (Функциональное Выражение)
+};
+
+function getFullPrice() {
+    return screenPrice + allServicePrices;        //Function Declaration (Объявление Функции)
+}
+
+function getTitle(string) {
+  return string.trim().charAt(0).toUpperCase() + string.slice(1);
+}
+
+function getServicePercentPrices(rollback) {
+    return fullPrice - (fullPrice * (rollback / 100));
+}
+
+
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+title = getTitle(title);
+servicePercentPrice = getServicePercentPrices(rollback);
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
+
+
+
+console.log(getRollbackMessage(fullPrice));
 console.log(screens.toLowerCase().split(", "));
-console.log(fullPrice * (rollback/100));
-
+console.log("Стоимость разработки сайта " + servicePercentPrice + " рублей");
